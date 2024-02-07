2cf98b8b11399340cc86f98167e3969cc91fbbc8
jose-tl <j.tonnesenlucas@gmail.com>
2024-02-07 13:42:58 +0100

    handin første opgave

--- hand-ins/app/src/main/java/com/example/handins/hand_in_1.kt
+++ hand-ins/app/src/main/java/com/example/handins/hand_in_1.kt
@@ -1,5 +1,14 @@
 package com.example.handins
+import java.util.Scanner
 
 fun main(){
     print("Hi")
+
+}
+
+fun checkAge(){
+    val reader = Scanner(System.`in`)
+    val userAge: Int = reader.nextInt()
+
+    if(userAge < 18) println("You cannot vote") else println("Vote NOW!")
 }
