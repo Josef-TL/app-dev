2cff00eda76ddd6ec98f82fa6189ffa4709c39a4
jose-tl <j.tonnesenlucas@gmail.com>
2024-02-22 23:22:19 +0100

    handin 3: opgave 2, opgave 4 og 4.5 completed

--- hand-ins/app/src/main/java/com/example/handins/handin3/classes.kt
+++ hand-ins/app/src/main/java/com/example/handins/handin3/classes.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import java.time.LocalDate
 import java.time.LocalDate.now
+import java.util.Calendar
 import java.util.Date
 
 // Opgave 1
@@ -15,6 +16,49 @@ class Article(private val title: String, private val author: String){
 }
 
 // Opgave 2
+
+interface FastFood{
+    var price: Int
+    var amount: Int
+    val type: String
+    fun orderFood(){}
+    fun calculatePrice(){}
+    fun addIngredient(){}
+
+}
+
+class Sandwich(): FastFood{
+    override var price = 43
+    override var amount = 1
+    override var type = "Sandwich"
+    override fun orderFood(){
+        println("$type is ordered $amount times.")
+    }
+    override fun calculatePrice(){
+        println("Order Price is ${price*amount}")
+    }
+    override fun addIngredient(){
+        println("$type has been added $amount times.")
+    }
+
+}
+
+class Pizza(): FastFood{
+    override var price = 43
+    override var amount = 1
+    override var type = "Pizza"
+    override fun orderFood(){
+        println("$type is ordered $amount times.")
+    }
+    override fun calculatePrice(){
+        println("Order Price is ${price*amount}")
+    }
+    override fun addIngredient(){
+        println("$type has been added $amount times.")
+    }
+}
+
+
 
 // Opgave 3
 interface Vehicle {
@@ -39,15 +83,33 @@ class Car: Vehicle{
 
 
 // Opgave 4
-class RedditPost( private val title: String, private val author: String) {
-    @RequiresApi(Build.VERSION_CODES.O)
-    val date = now()
+class RedditPost(val title: String, val author: String, var frontPage: RedditFrontPage) {
+
+    val currentDate = Date()
     var voteBalance: Int = 1
 
-
-// opgave 5
-
-
-
+    fun upVote(){
+        voteBalance++
+        println("Post upvoted. It now has $voteBalance upvotes")
+    }
+    fun downVote(){
+        voteBalance--
+        println("Post downvoted. It now has $voteBalance upvotes")
+    }
+    init {
+        frontPage.redditPostList.add(this)
+    }
 }
 
+// Opgave 4.5
+class RedditFrontPage(){
+    var redditPostList = mutableListOf<RedditPost>()
+
+    fun removePostFromIndex(index: Int){
+        redditPostList.removeAt(index)
+    }
+
+    fun sortPosts(){
+        redditPostList.sortByDescending { it.voteBalance }
+    }
+}
--- hand-ins/app/src/main/java/com/example/handins/handin3/hand_in_3.kt
+++ hand-ins/app/src/main/java/com/example/handins/handin3/hand_in_3.kt
@@ -1,5 +1,7 @@
 package com.example.handins.handin3
 
+import java.util.Calendar
+
 fun main(){
    // Opgave 1
     val arti1 = Article("Echoes of Eternity: Unraveling the Mysteries of Time","Dr. Cassandra Wells")
@@ -13,6 +15,20 @@ fun main(){
     print(list1.toString())
 
     // opgave 2
+    val pizza1 = Pizza()
+    val pizza2 = Pizza()
+    val sandwich1 = Sandwich()
+    val sandwich2 = Sandwich()
+    val sandwich3 = Sandwich()
+
+    val foodList = arrayListOf<FastFood>(pizza1,pizza2,sandwich1,sandwich2,sandwich3)
+
+    for (item in foodList){
+        item.price += foodList.indexOf(item)
+        item.amount += foodList.indexOf(item)
+        item.calculatePrice()
+        item.orderFood()
+    }
 
     // opgave 3
     val lambo = Car()
@@ -21,15 +37,36 @@ fun main(){
     ferrari.changeGear(6)
 
     //opgave 4
+    val fronter = RedditFrontPage()
+    val redPost1 = RedditPost("Why is corona dead?", "DeadBeat-dxd", fronter)
+    val redPost3 = RedditPost("How to find milk", "daddy shortlegs", fronter)
+    val redPost2 = RedditPost("Bring my father back", "xXx Dom1n4t0R xXx", fronter)
 
+    for(i in 1..10){
+        redPost2.upVote()
+    }
+    for(i in 1..100){
+        redPost3.upVote()
+    }
+
+    println(fronter.redditPostList[1].voteBalance)
+    fronter.sortPosts()
+    println(fronter.redditPostList[0].voteBalance)
+
+    println(fronter.redditPostList)
+    fronter.removePostFromIndex(1)
+    println(fronter.redditPostList)
+
+
+
+
+    // Opgave 5
     val words = listOf("apple", "banana", "apple", "orange", "banana", "apple", "orange", "banana", "apple","banana", "apple", "orange", "banana","banana", "apple", "orange", "apple", "orange", "banana")
-
     println(countUnique(words))
 }
 
 
 // Opgave 5
-
 fun countUnique(list: List<String>): MutableMap<String,Int>{
     val distinctList = list.distinct()
     var retMap = mutableMapOf<String, Int>()
